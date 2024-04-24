8b1cbd4a76a91b7bfe29472a97e2adb60c0043fd
Alvaro <aimc2918@hotmail.com>
2024-04-24 10:01:55 -0400

    working

--- Tests/Code/python_code/grainserver.py
+++ Tests/Code/python_code/grainserver.py
@@ -41,7 +41,51 @@ camera_config = picam2.create_preview_configuration()
 picam2.configure(camera_config) 
 picam2.start_preview(Preview.NULL)
 
+def guardar_foto():
+    try:
+        while True:
+            hora_santiago = datetime.datetime.now()  # Obtiene la hora actual
+            fecha_hora_actual = hora_santiago.strftime("%H%M%S_%d%m%Y")            
+            nombre_foto = f"foto_up_{fecha_hora_actual}.jpg"
+
+            with open(nombre_foto, "ab") as f:  # Modo "ab" para escritura binaria y anexar
+                f.write(data)
+                print(f"[{hora_santiago.strftime('%H:%M:%S de %d/%m/%Y')}] --- Foto guardada como {nombre_foto}")
+            
+            foto_id = f"foto_up_{fecha_hora_actual}.jpg"
+            datos_a_csv("", foto_id)
+    
+    except Exception as e:
+        print("Error al recibir la foto:", e)
+
 def guardar_datos_sensor():
+    try:
+        while True:
+            data = ",".join(map(str, datos_sensor()))  # Obtener datos del sensor y convertirlos a cadena
+            hora_santiago = datetime.datetime.now(zona_santiago)
+
+            foto_id = f"foto_up_{hora_santiago.strftime("%H%M%S_%d%m%Y")}.jpg"
+            datos_a_csv(data, foto_id)
+
+            lcd.lcd_display_string("Datos guardados:", 1)
+            lcd.lcd_display_string(f"{hora_santiago.strftime('%H:%M:%S')}", 2)
+            print(f"[{hora_santiago.strftime('%H:%M:%S de %d/%m/%Y')}] --- Datos del sensor guardados")
+
+            time.sleep(60)  # Envía datos cada 3 segundos
+
+    except Exception as e:
+        print("Error sending data:", e)
+
+def datos_a_csv(datos, foto_id):
+    datos = datos.split(",")
+
+    fecha_hora = datetime.datetime.now().strftime("%Y-%m-%d,%H:%M:%S")
+    datos.append(foto_id)
+
+    with open("data_test.csv", mode='a', newline='') as archivo_csv:
+        escritor_csv = csv.writer(archivo_csv)
+        escritor_csv.writerow([fecha_hora] + datos)
+
 
 # Definición bar_graph()
 # Obtiene las lecturas de los 10+1 canales
