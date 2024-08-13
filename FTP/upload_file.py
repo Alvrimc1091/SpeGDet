8a0fcdbfdb2bfc31ff18294ff72ab41d313c79a0
Alvrimc1091 <aimc2918@hotmail.com>
2024-08-13 16:05:31 -0400

    upload ftp files

---
+++ FTP/upload_file.py
@@ -0,0 +1,23 @@
+from ftplib import FTP
+
+# FTP server details
+ftp_server = "192.168.0.50"
+port = 2121
+username = "aimc"
+password = "qwerty"
+filename = "photofile.txt"  # File to upload
+remote_path = "photofile.txt"  # Path on the FTP server
+
+# Connect to the FTP server
+ftp = FTP()
+ftp.connect(ftp_server, port)
+ftp.login(user=username, passwd=password)
+
+# Upload the file
+with open(filename, 'rb') as file:
+    ftp.storbinary(f'STOR {remote_path}', file)
+
+print(f"Uploaded {filename} to {remote_path}")
+
+# Close the connection
+ftp.quit()
